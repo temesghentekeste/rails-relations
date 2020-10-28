a601a319b07e3c2034fb5328c67016158efb730e
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-28 13:46:26 +0300

    Add dependent option

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.1'
+gem 'faker'
 gem 'hirb'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do 
+  
+  #creating user
+  User.destroy_all
+  10.times do 
+    User.create(name: Faker::FunnyName.name)
+  end
+
+  Post.destroy_all
+  10.times do 
+    Post.create(body: Faker::Lorem.sentence(word_count: 3), user_id: User.all.sample.id)
+  end
+end
+
